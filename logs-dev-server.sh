c16b470005cd45beaa66a1d1c0853630492e7528
a-Osipow-s <toni.osipow@mail.ru>
2018-12-18 21:37:38 +0300

    angular-django-docker create 'sh' file

---
+++ logs-dev-server.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker-compose -f backend/docker-compose.yml logs -f
---
+++ run-dev-server-client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker-compose -f backend/docker-compose.yml up -d
+docker-compose -f frontend/docker-compose.yml up -d
